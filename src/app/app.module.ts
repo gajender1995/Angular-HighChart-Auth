b309ae31d13039e688ef159468c7b418ef44f969
gajender1995 <gajendersingh1995@gmail.com>
2018-11-02 09:40:07 +0530

    update chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { routing } from './app.route';
 import { AuthService } from './login/auth.service';
 import { UserComponent } from './user/user.component';
-import {  HttpClientModule  } from '@angular/common/http';
-import { ChartService  } from './user/chart.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartService } from './user/chart.service';
 
 import { ChartModule } from 'angular2-highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
+declare var require: any;
+
+export function highchartsFactory() {
+  const hc = require('highcharts');
+  const dd = require('highcharts/modules/drilldown');
+  dd(hc);
+
+  return hc;
+}
+
 
 @NgModule({
   declarations: [
@@ -21,10 +33,13 @@ import { ChartModule } from 'angular2-highcharts';
     BrowserModule,
     routing,
     FormsModule,
-    HttpClientModule, 
-    ChartModule.forRoot(require('highcharts'),
+    HttpClientModule,
+    ChartModule
   ],
-  providers: [AuthService,ChartService],
+  providers: [AuthService, ChartService, {
+    provide: HighchartsStatic,
+    useFactory: highchartsFactory
+  }],
   bootstrap: [AppComponent]
-}) 
-export class AppModule { }
+})
+export class AppModule {}
